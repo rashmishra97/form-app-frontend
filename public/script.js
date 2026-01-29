6dbc2e89e5a774fa5dcb4ae9960be1616c110823
Rashmi Mishra <rashmimishra97@gmail.com>
2026-01-29 18:05:11 +0530

    port chnage to private IP

--- public/script.js
+++ public/script.js
@@ -11,7 +11,7 @@ if (form) {
       contact: document.getElementById('contact').value
     };
 
-    await fetch('http://127.0.0.1:5001/api/submit', {
+    await fetch('http://172-31-41-153:5001/api/submit', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload)
