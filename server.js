739b27afd846234957f1f285121c6ccaa076785a
Rashmi Mishra <rashmimishra97@gmail.com>
2026-01-29 12:42:07 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = 3001;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("frontend Server running on 0.0.0.0", PORT, "HAIN");
+});
+
+
